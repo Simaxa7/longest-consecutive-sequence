d91316e737f3fb09bf971661e6cf61b6d0a558ba
“Simaxa7” <simaxa7@mail.ru>
2018-10-01 18:15:51 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+  if ( !array.length ) return 0;
+    
+  let obj = {};
+  let rezultCount = 1;
+
+  for (let i = 0; i < array.length; i++) {
+
+    let countRight = 0;
+    let rightNumber = 0;
+    let countLeft = 0;
+    let leftNumber = 0;
+
+    let myCount = 1;
+    let x = String(array[i]);
+    let key = array[i];
+
+    if( obj[x] == undefined) {
+
+      rightNumber = Number(x) + 1;
+
+      if( obj[rightNumber] >=1 ) {
+        countRight = obj[rightNumber];
+      }
+
+      leftNumber  = Number(x) - 1;
+
+      if( obj[leftNumber] >= 1 ) {
+          countLeft = obj[leftNumber];
+      }
+
+      myCount +=  countRight + countLeft;
+
+      if (countRight) {
+        obj[key + countRight] = myCount;
+      }
+
+      if (countLeft) {
+        obj[key - countLeft] = myCount;
+        obj[leftNumber] = myCount;
+      }
+
+      obj[key] = myCount;
+
+      if(myCount > rezultCount){
+        rezultCount = myCount;
+      }
+    } 
+  }
+
+  return rezultCount
 }
